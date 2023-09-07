41b6f1737698fb85b80b8c829d9632570f785e7f
Pradeepkb241 <pradeepbanjare11@gmail.com>
2023-09-07 15:49:05 +0530

    background change

--- dom.js
+++ dom.js
@@ -8,3 +8,9 @@ item.style.color = 'green';
 
 let list = document.getElementsByClassName('list-group-item');
 list[2].style.backgroundColor ='green';
+
+
+for(let i = 0; i <list.length;i++){
+    list[i].style.backgroundColor = 'yellow';
+    list[i].style.fontWeight='bold';
+}
