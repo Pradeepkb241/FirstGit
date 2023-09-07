70a4fcd2d61addec32056436d6089817b2e795af
Pradeepkb241 <pradeepbanjare11@gmail.com>
2023-09-07 14:14:34 +0530

    Second commit

---
+++ dom.js
@@ -0,0 +1,6 @@
+let header = document.getElementById('main-header');
+header.style.border = '4px solid black'
+
+let item = document.getElementById('addItem');
+item.style.fontWeight = 'bold';
+item.style.color = 'green';
